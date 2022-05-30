0a1c38769dcc3d980fc081a4c91b348ddabf7099
Laressa Moraes <laressa.luisy@hotmail.com>
2022-05-30 19:08:55 -0300

    Project creation

---
+++ src/main/java/com/grupowl/breakfastproject/BreakfastprojectApplication.java
@@ -0,0 +1,13 @@
+package com.grupowl.breakfastproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfastprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupowl/breakfastproject/BreakfastprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupowl.breakfastproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
